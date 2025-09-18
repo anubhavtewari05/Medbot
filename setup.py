16c4679165db765f627718133ce93b288b620971
anubhavtewari05 <anubhavtewari05@gmail.com>
2025-09-18 20:17:38 +0530

    Requirements updated

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup 
+
+setup(
+    name="medbot",
+    version="0.1.0",
+    author="Anubhav Tewari",
+    author_email="anubhavtewari05@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
